745e7f030a35718ad41a34f942557549f683579c
Philip Guther <philip.guther@gmail.com>
2015-05-03 15:36:45 -0700

    Gson Chore class

---
+++ app/src/main/java/com/HouseMateApps/housemate/Chore.java
@@ -0,0 +1,17 @@
+package com.HouseMateApps.housemate;
+
+/**
+ * Created by Philip on 5/3/2015.
+ */
+public class Chore {
+    int id;
+    String day;
+    String name;
+    boolean assignable;
+    String assignedTo;
+    boolean completed;
+    boolean claimable;
+    String claimedBy;
+
+    Chore(){}
+}
