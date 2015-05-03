32cf2692d9189b99c1bb064032b102687482f2e8
Philip Guther <philip.guther@gmail.com>
2015-05-03 15:20:50 -0700

    Changed package name to official group name

--- app/src/androidTest/java/com/HouseMateApps/housemate/ApplicationTest.java
+++ app/src/androidTest/java/com/HouseMateApps/housemate/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.robertlyons.housemate;
+package com.HouseMateApps.housemate;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/HouseMateApps/housemate/MainActivity.java
+++ app/src/main/java/com/HouseMateApps/housemate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.robertlyons.housemate;
+package com.HouseMateApps.housemate;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
